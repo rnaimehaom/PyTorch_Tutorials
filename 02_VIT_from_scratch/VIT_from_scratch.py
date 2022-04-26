db85a7132fdfddd1cc8498ea0f553943263d5649
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-04-26 15:23:27 +0000

    Started MLP block

--- 02_VIT_from_scratch/VIT_from_scratch.py
+++ 02_VIT_from_scratch/VIT_from_scratch.py
@@ -16,4 +16,25 @@ class Residual(nn.Module):
         return self.fn(x, **kwargs) + x
 
 # Create normalisation layer class
-class LayerNormalise(nn.Module)
+class LayerNormalise(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+# Create the MLP for the fully connected layer
+class MLP_Block(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.1):
+        super().__init__()
+        self.nn1 = nn.Linear(dim, hidden_dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.normal_(self.nn1.bias, std = 1e-6)
+        self.af1 = nn.GELU()
+        self.do1 = nn.Dropout(dropout)
+        self.nn2 = nn.Linear(hidden_dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn2.weight)
+        torch.nn.init.normal_(self.nn2.bias, std = 1e-6)
+        self.do2 = nn.Dropout(dropout)
