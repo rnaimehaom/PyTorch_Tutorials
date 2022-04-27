b325581a3b08a3381b20535f4e7f9b2bf3f08d45
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-04-27 11:44:01 +0000

    Created the transformer class

--- 02_VIT_from_scratch/VIT_from_scratch.py
+++ 02_VIT_from_scratch/VIT_from_scratch.py
@@ -84,3 +84,18 @@ class Attention(nn.Module):
 
 
 # Create the transformer block
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, mlp_dim, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(LayerNormalise(dim, Attention(dim, heads=heads, dropout=dropout))),
+                Residual(LayerNormalise(dim, MLP_Block(dim, mlp_dim, dropout=dropout)))
+            ]))
+    
+    def forward(self, x, mask=None):
+        for attention, mlp in self.layers: 
+            x = attention(x, mask=mask) #go to attention
+            x = mlp(x)
+        return x
