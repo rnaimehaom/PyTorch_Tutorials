7028d12b3c39f343fcd1a7fa668f503c1c700f33
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-04-27 11:31:32 +0000

    Added attention head class

--- 02_VIT_from_scratch/VIT_from_scratch.py
+++ 02_VIT_from_scratch/VIT_from_scratch.py
@@ -46,3 +46,41 @@ class MLP_Block(nn.Module):
         x = self.nn2(x)
         x = self.do2(x)
         return x
+
+# Create the attention head
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dropout=0.1):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim ** -0.5 #1/sqrt(dim)
+        self.to_qkv = nn.Linear(dim, dim *3, bias=True) #Weight query, key and value for each vector
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.zeros_(self.nn1.bias)
+        self.nn1 = nn.Linear(dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.zeros_(self.nn1.bias)
+        self.do1 = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        b,n,_, h = *x.shape, self.heads
+        qkv = self.to_qkv(x) # gets q = Q = Wq matmul x1, k=Wk nm x2, v= Wv mm x3
+        q,k,v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv = 3, h = h) # split into multi head attentions
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1,0), value=True)
+            assert mask.shape[-1] == dots.shape[-1], '[MASK DIM ERROR] the mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:,:, None]
+            dots.masked_fill_(~mask, float('-inf'))
+            del mask
+
+        attn = dots.softmax(dim=1)
+        out = torch.einsum('bhij,bhjd->bhid', attn, v) #product of v times whatever inside softmax
+        out = rearrange(out, 'b h n d -> b n (h d)') #concat heads into one matrix, ready for next encoder block
+        out =  self.nn1(out)
+        out = self.do1(out)
+        return out
+
+
+# Create the transformer block
