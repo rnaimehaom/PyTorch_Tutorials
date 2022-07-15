89e8c12d9072cecdca8c7cbd2ec6b7071b0f8ecd
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-07-15 15:28:03 +0000

    Added mlp regression case

--- 03_MLP_Regression/mlp_regression.py
+++ 03_MLP_Regression/mlp_regression.py
@@ -5,12 +5,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#=====================================================================================
+# Data Loading
+#=====================================================================================
 
 # Read in the medical insurance data
 df = pd.read_csv('https://raw.githubusercontent.com/StatsGary/Data/main/insurance.csv')
-print(df.head())
-print(df['charges'].describe())
 
+#=====================================================================================
+# Feature Engineering
+#=====================================================================================
 # Encode the categorical features
 cat_cols = ['sex', 'smoker', 'region', 'children']
+cont_cols = ['age', 'bmi']
 
+# Set the target (y) column
+y = ['charges']
+
+# CONVERT CATEGORICAL COLUMNS
+for cat in cat_cols:
+    df[cat] = df[cat].astype('category')
+
+# Get the cat codes for each of the categorical values
+cats = np.stack([df[col].cat.codes.values for col in cat_cols],axis=1)
+
+# Convert this to a tensor
+cats = torch.tensor(cats, dtype=torch.int64)
+print(cats)
+
+# CONVERT CONTINUOUS VARIABLES
+cont_cols = np.stack([df[col].values for col in cont_cols], axis=1)
+cont_cols = torch.tensor(cont_cols, dtype=torch.float)
+print(cont_cols)
+
+#CONVERT TARGET (Y) LABEL
+y= torch.tensor(df[y].values,dtype=float)
+print(cats.shape, cont_cols.shape, y.shape)
