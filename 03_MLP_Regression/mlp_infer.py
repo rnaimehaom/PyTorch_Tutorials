b9db97c0928e8506b8697abdea53e5d69c19ef9a
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-07-25 13:03:34 +0000

    Added inference script

--- 03_MLP_Regression/mlp_infer.py
+++ 03_MLP_Regression/mlp_infer.py
@@ -6,10 +6,47 @@ import pandas as pd
 #Custom model imports
 from models.Regression import MLPRegressor
 
-#Use embedding sizes from training step
-emb_szs = [(2, 1), (2, 1), (4, 2), (6, 3)]
+data_name ='medical_insurance'
+# Read in the medical insurance data
+df = pd.read_csv('https://raw.githubusercontent.com/StatsGary/Data/main/insurance_prod.csv')
+# Drop nulls
+df.dropna(axis='columns',inplace=True)
+# Get number of rows
+obs = len(df)
+
+#=====================================================================================
+# Feature Engineering
+#=====================================================================================
+# Encode the categorical features
+cat_cols = ['sex', 'smoker', 'region', 'children']
+cont_cols = ['age', 'bmi']
+
+# Set the target (y) column
+y = ['charges']
+
+# CONVERT CATEGORICAL COLUMNS
+for cat in cat_cols:
+    df[cat] = df[cat].astype('category')
+
+cats = np.stack([df[col].cat.codes.values for col in cat_cols], 1)
+cats = torch.tensor(cats, dtype=torch.int64)
+# Convert continuous variables to a tensor
+conts = np.stack([df[col].values for col in cont_cols], 1)
+conts = torch.tensor(conts, dtype=torch.float)
+# Create outcome
+y = torch.tensor(df[y].values, dtype=torch.float).reshape(-1,1)
+# Set embedding sizes
+cat_szs = [len(df[col].cat.categories) for col in cat_cols]
+emb_szs = [(size, min(50, (size+1)//2)) for size in cat_szs]
+
+print(emb_szs)
 
 # Instantiate inference model
-model_infer = MLPRegressor(emb_szs, 2, 1, [200,100], p=0.4)
+model_infer = MLPRegressor(emb_szs, conts.shape[1], 1, [200,100], p=0.4)
 model_infer.load_state_dict(torch.load('model_artifacts/medical_insurance_400.pt'))
 print(model_infer.eval())
+
+
+
+
+
--- 03_MLP_Regression/mlp_train.py
+++ 03_MLP_Regression/mlp_train.py
@@ -10,6 +10,8 @@ import time
 import seaborn as sns
 from datetime import datetime as dt
 
+from models.Regression import MLPRegressor
+
 #=====================================================================================
 # Data Loading
 #=====================================================================================
@@ -56,41 +58,6 @@ cat_szs = [len(df[col].cat.categories) for col in cat_cols]
 emb_szs = [(size, min(50, (size+1)//2)) for size in cat_szs]
 print(emb_szs)
 print(conts.shape[1])
-
-class MLPRegressor(nn.Module):
-
-    def __init__(self, emb_szs, n_cont, out_sz, layers, p=0.5):
-        super().__init__()
-        self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
-        self.emb_drop = nn.Dropout(p)
-        self.bn_cont = nn.BatchNorm1d(n_cont)
-        
-        layerlist = []
-        n_emb = sum((nf for ni,nf in emb_szs))
-        n_in = n_emb + n_cont
-        
-        for i in layers:
-            layerlist.append(nn.Linear(n_in,i)) 
-            layerlist.append(nn.ReLU(inplace=True))
-            layerlist.append(nn.BatchNorm1d(i))
-            layerlist.append(nn.Dropout(p))
-            n_in = i
-        layerlist.append(nn.Linear(layers[-1],out_sz))
-            
-        self.layers = nn.Sequential(*layerlist)
-    
-    def forward(self, x_cat, x_cont):
-        embeddings = []
-        for i,e in enumerate(self.embeds):
-            embeddings.append(e(x_cat[:,i]))
-        x = torch.cat(embeddings, 1)
-        x = self.emb_drop(x)
-        
-        x_cont = self.bn_cont(x_cont)
-        x = torch.cat([x, x_cont], 1)
-        x = self.layers(x)
-        return x
-
 
 # Use the model
 torch.manual_seed(123)
