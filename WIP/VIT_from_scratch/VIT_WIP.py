954164051cb097de4388372947ac3a1401846e91
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-08-09 13:10:09 +0000

    Started training loop for VIT example

--- WIP/VIT_from_scratch/VIT_WIP.py
+++ WIP/VIT_from_scratch/VIT_WIP.py
@@ -15,3 +15,28 @@ torch.manual_seed(123)
 
 # Setup the function 
 
+def main():
+    # Load data
+    transform = ToTensor()
+    
+    train_set = MNIST('./../datasets', train=True, download=True, transform=transform)
+    test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
+    
+    train_dataloader = DataLoader(train_set, shuffle=True, batch_size=16)
+    test_dataloader = DataLoader(test_set, shuffle=False, batch_size=16)
+    
+    # Define the model
+    
+    model = ...
+    N_EPOCHS=5
+    LR=0.01
+    
+    # Training loop
+    optimizer = Adam(model.parameters(), lr=LR)
+    criterion = CrossEntropyLoss()
+    
+    for epoch in range(N_EPOCHS):
+        train_loss = 0.0
+        # Iterate through batches
+        
+    
