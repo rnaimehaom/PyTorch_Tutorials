acc0a905a01a9de4e09b663b34258c7eebfa0384
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-09-07 13:38:28 +0000

    PyTorch maximisation added to

--- 04_AUROC_optimisation/1_optim_with_linear_models.py
+++ 04_AUROC_optimisation/1_optim_with_linear_models.py
@@ -5,17 +5,20 @@ Script Name:    1_optim_with_linear_models.py
 Author:         Gary Hutson
 Date:           05/09/2022
 Usage:          python 1_optim_with_linear_models.py
+Credits:        Code adapted from Erik Drysdale
 """
 
 # Define imports
 import pandas as pd
-from sklearn.datasets import load_boston
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 from scipy.optimize import minimize
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # Bring in custom imports
 from AUROCker.functions import sigmoid, AUROC, convex_AUROC, index_I0I1, derivative_chained_AUROC
 
@@ -24,19 +27,21 @@ TOP_PROP = 0.9
 SPLIT_PROP = 0.2
 
 # Load in test dataset
-X, y = load_boston(return_X_y=True)
+
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+X = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+y = raw_df.values[1::2, 2]
 # Binarise the y outcome variable
 y = np.where(y > np.quantile(y,TOP_PROP), 1,0)
 
 # Simulate a number of datasets
-num_to_sim = 200
+num_to_sim = 100
 auc_list = []
-weight_list = []
+results_list = []
 winit = np.repeat(0, X.shape[1])
-print(winit)
 
 # Initialise a loop to loop through our simulations
-
 for sim in range(num_to_sim):
     y_train, y_test, X_train, X_test = train_test_split(y, X,
                                                         test_size=SPLIT_PROP,
@@ -48,7 +53,7 @@ for sim in range(num_to_sim):
     idx0_test, idx1_test = index_I0I1(y_test)
     
     # Optimised AUC
-    optim_AUC = minimize(func=convex_AUROC, x0=winit,
+    optim_AUC = minimize(fun=convex_AUROC, x0=winit,
                          args=(scaled.transform(X_train),
                                idx0_train, idx1_train),
                          method='L-BFGS-B', 
@@ -57,9 +62,38 @@ for sim in range(num_to_sim):
     eta_auc = scaled.transform(X_test).dot(optim_AUC)
     logit_mod = LogisticRegression(penalty='none')
     probs_logit = logit_mod.fit(scaled.transform(X_train), y_train).predict_proba(X_test)[:,1]
+    auc1, auc2 = roc_auc_score(y_test, eta_auc), roc_auc_score(y_test, probs_logit)
+    auc_list.append([auc1, auc2])
+    # results_list.append(pd.DataFrame(
+    #     {'cn':load_boston()['feature_names'],
+    #      'auc':optim_AUC,'logit':logit_mod.coef_.flatten()}
+    # ))
     
-    
-    
+auc_diff = np.vstack(auc_list).mean(axis=0)
+print('AUC from convex AUROC: %0.2f%%\nAUC for LogisticRegression: %0.2f%%' % 
+      (auc_diff[0], auc_diff[1]))
 
 
+# Visualise the difference
+scaler = StandardScaler().fit(X)
+idx0, idx1 = index_I0I1(y)
+optim_cAUC = minimize(fun=convex_AUROC, x0=winit,
+                      args=(scaler.transform(X), idx0, idx1),
+                      method='L-BFGS-B', jac=derivative_chained_AUROC).x
+eta_auc = scaler.transform(scaler.transform(X)).dot(optim_cAUC)
+logistic_mod = LogisticRegression(max_iter=1e3).fit(scaler.transform(scaler.transform(X)),
+                                                    y).predict_log_proba(scaler.transform(X))[:,1]
 
+output_dataframe = pd.DataFrame({
+    'y':y, 
+    'Logistic': logistic_mod, 
+    'cAUROC': eta_auc
+}).melt('y')
+
+print(output_dataframe)
+
+# Create facetgrid to compare the AUROC outputs
+graph = sns.FacetGrid(data=output_dataframe, hue='y', col='variable', sharex=False, sharey=False, height=5)
+graph.map(sns.distplot, 'value')
+graph.add_legend()
+plt.savefig('output.png')
--- 04_AUROC_optimisation/2_AUROC_maximisation_with_PyTorch.py
+++ 04_AUROC_optimisation/2_AUROC_maximisation_with_PyTorch.py
@@ -11,6 +11,7 @@ Credits:        Code adapted from Erik Drysdale
 # Define imports
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import fetch_california_housing
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -18,7 +19,9 @@ import numpy as np
 from scipy.optimize import minimize
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 # Bring in custom imports
 from AUROCker.functions import sigmoid, AUROC, convex_AUROC, index_I0I1, derivative_chained_AUROC
 
@@ -27,3 +30,54 @@ TOP_PROP = 0.9
 SPLIT_PROP = 0.2
 
 # Load in test dataset
+data = fetch_california_housing(download_if_missing=True)
+feat_names_cali = data.feature_names
+print(feat_names_cali)
+
+# Split data 
+X = data.data
+y = data.target
+y += np.random.randn(y.shape[0])*(y.std())
+y = np.where(y > np.quantile(y, 0.95),1,0)
+
+# Create the splits
+y_train, y_test, X_train, X_test = train_test_split(y, X, test_size=SPLIT_PROP, stratify=y)
+scaled = StandardScaler().fit(X_train)
+
+
+# Create the feedforward network 
+class feedy(nn.Module):
+    def __init__(self, num_features):
+        super(feedy, self).__init__()
+        feats = num_features
+        self.fc1 = nn.Linear(feats, 36)
+        self.fc2 = nn.Linear(36,12)
+        self.fc3 = nn.Linear(12,6)
+        self.fc4 = nn.Linear(6,1)
+        
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return X
+    
+# Binary loss function
+criterion = nn.BCEWithLogitsLoss()
+nnet = feedy(num_features=X.shape[1])
+optimizer = torch.optim.Adam(params=nnet.parameters(), lr=0.001)
+
+# Validation and training samples
+y_held, y_valid, x_held, x_valid = train_test_split(y_train, X_train, test_size=SPLIT_PROP, stratify=y_train)
+scaler = StandardScaler().fit(x_held)
+idx0_held, idx1_held = index_I0I1(y_held)
+
+# Set the epochs
+epochs = 100
+auc_history = []
+
+for epoch in range(epochs):
+    print('[EPOCH] {} of {}'.format(epoch + 1, epochs))
+    # Sample class 0 pairs
+    idx0_epoch = np.random.choice(idx0_held)
+    
